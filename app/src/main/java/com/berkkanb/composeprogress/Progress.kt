8594ed0450f95027363f14bfc387fe16443ca8ea
berkkanB <butun18@itu.edu.tr>
2022-11-17 13:05:05 +0300

    Remove basic progress, stepImage added,drawIndicators function added to DrawScope

--- app/src/main/java/com/berkkanb/composeprogress/Progress.kt
+++ app/src/main/java/com/berkkanb/composeprogress/Progress.kt
@@ -12,11 +12,11 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.vectorResource
 
 @Composable
 fun MyProgress(
@@ -28,12 +28,15 @@ fun MyProgress(
     indicatorColor: Color = Color.White,
     indicatorWidth: Float = 5f,
     cornerRadius: CornerRadius = CornerRadius.Zero,
+    stepImageVector: ImageVector? = null,
+    stepImageVectorMultiplier: Float = 0.6f,
     animationDuration: Int,
-    animationDelay:Int = 0
+    animationDelay: Int = 0
 ) {
     val targetPercentage = remember {
         Animatable(0f)
     }
+    val painter = stepImageVector?.let { rememberVectorPainter(image = it) }
     LaunchedEffect(key1 = fillProgressBy) {
         targetPercentage.animateTo(
             targetValue = fillProgressBy,
@@ -45,6 +48,7 @@ fun MyProgress(
         )
     }
     Canvas(modifier = modifier) {
+        val canvasSize = this.size
         drawRoundRect(
             color = progressBackgroundColor,
             cornerRadius = cornerRadius
@@ -57,68 +61,48 @@ fun MyProgress(
             ),
             cornerRadius = cornerRadius
         )
-        indicators?.forEach {
-            drawLine(
-                color = indicatorColor,
-                start = Offset(x = it * this.size.width, y = 0f),
-                end = Offset(x = it * this.size.width, y = this.size.height),
-                strokeWidth = indicatorWidth
-            )
+        indicators?.let { indicatorList ->
+            drawIndicators(fillProgressBy,indicatorList,indicatorColor,indicatorWidth,canvasSize,stepImageVectorMultiplier,painter)
         }
-        /*
-        val path = Path().apply {
-            addRoundRect(
-                RoundRect(
-                    rect = Rect(
-                        offset = Offset(0f, 0f),
-                        size = Size(
-                            width = targetPercentage.value * this@Canvas.size.width,
-                            height = this@Canvas.size.height
-                        )
-                    ),
-                    bottomLeft = cornerRadius,
-                    topLeft = cornerRadius,
-                )
-            )
-        }
-        drawPath(path, color = progressColor)*/
+
     }
 }
 
-@Composable
-fun MyProgress(
-    modifier: Modifier,
-    progressColor: Color,
-    progressBackgroundColor: Color,
-    fillProgressBy: Float,
-    indicators: List<Float>? = null,
-    indicatorColor: Color = Color.White,
-    indicatorWidth: Float = 5f,
-    cornerRadius: CornerRadius = CornerRadius.Zero
+fun DrawScope.drawIndicators(
+    filledProgressValue:Float,
+    indicatorList: List<Float>,
+    indicatorColor: Color,
+    indicatorWidth: Float,
+    canvasSize:Size,
+    stepImageVectorMultiplier: Float = 0f,
+    painter: VectorPainter? = null
 ) {
-
-    Canvas(modifier = modifier) {
-        drawRoundRect(
-            color = progressBackgroundColor,
-            cornerRadius = cornerRadius
+    indicatorList.forEachIndexed { index, indicator ->
+        drawLine(
+            color = indicatorColor,
+            start = Offset(x = indicator * this.size.width, y = 0f),
+            end = Offset(x = indicator * this.size.width, y = this.size.height),
+            strokeWidth = indicatorWidth
         )
-        drawRoundRect(
-            color = progressColor,
-            size = Size(
-                width = fillProgressBy * this.size.width,
-                height = this.size.height
-            ),
-            cornerRadius = cornerRadius
-        )
-        indicators?.forEach {
-            drawLine(
-                color = indicatorColor,
-                start = Offset(x = it * this.size.width, y = 0f),
-                end = Offset(x = it * this.size.width, y = this.size.height),
-                strokeWidth = indicatorWidth
-            )
+        painter?.let { vectorPainter ->
+            with(vectorPainter) {
+                val size = (canvasSize.height * stepImageVectorMultiplier)
+                val previousLeft = if (index == 0) 0f else indicatorList[index - 1]
+                val left = ((indicator + previousLeft) * canvasSize.width - size) / 2
+                val top = (canvasSize.height - size) / 2
+                if (indicator*canvasSize.width <= filledProgressValue*canvasSize.width){ //TODO LAST PIECE CHECKMARK
+                    translate(left = left, top = top) {
+                        draw(
+                            Size(size, size)
+                        )
+                    }
+                }
+            }
         }
-        /*
+    }
+}
+
+/*
         val path = Path().apply {
             addRoundRect(
                 RoundRect(
@@ -135,5 +119,3 @@ fun MyProgress(
             )
         }
         drawPath(path, color = progressColor)*/
-    }
-}
