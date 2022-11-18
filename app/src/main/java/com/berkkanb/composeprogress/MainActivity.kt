3746c6b11d09fe75bb34df239e027d50f7c491cf
berkkanB <butun18@itu.edu.tr>
2022-11-18 10:40:17 +0300

    Main activity sample added

--- app/src/main/java/com/berkkanb/composeprogress/MainActivity.kt
+++ app/src/main/java/com/berkkanb/composeprogress/MainActivity.kt
@@ -3,6 +3,7 @@ package com.berkkanb.composeprogress
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,12 +12,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.berkkanb.composeprogress.ui.theme.ComposeProgressTheme
+import com.berkkanb.lib.BarProgress
+import com.berkkanb.lib.StepProgress
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,41 +37,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    var state by remember {
+                        mutableStateOf(1f)
+                    }
                     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                         Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-                            MyProgress(
+                            BarProgress(
                                 modifier = Modifier
                                     .height(30.dp)
-                                    .width(200.dp),
+                                    .width(200.dp)
+                                    .clickable { state = Random.nextFloat() },
                                 progressColor = Color(0xFF6633CC),
                                 progressBackgroundColor = Color(0xFFE0E0E0),
-                                indicators = listOf(0.1f,0.2f,0.3f,0.6f),
-                                fillProgressBy = 0.77f,
-                                cornerRadius = CornerRadius(30f,30f),
+                                indicators = listOf(0.1f, 0.4f, 0.6f, 0.8f),
+                                fillProgressBy = state,
+                                cornerRadius = CornerRadius(30f, 30f),
+                                stepImageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_check_24),
                                 animationDuration = 700,
                                 animationDelay = 0
                             )
-                            MyProgress(
+                            BarProgress(
                                 modifier = Modifier
                                     .height(30.dp)
                                     .width(200.dp),
                                 progressColor = Color(0xFF6633CC),
                                 progressBackgroundColor = Color(0xFFE0E0E0),
-                                indicators = listOf(0.3f,0.5f,0.7f,0.85f),
-                                fillProgressBy = 0.95f,
-                                cornerRadius = CornerRadius(30f,30f),
+                                indicators = listOf(0.3f, 0.5f, 0.7f, 0.85f),
+                                fillProgressBy = 0.85f,
+                                cornerRadius = CornerRadius(30f, 30f),
                                 animationDuration = 700,
                                 animationDelay = 200
                             )
-                            MyProgress(
+                            BarProgress(
                                 modifier = Modifier
                                     .height(30.dp)
                                     .width(200.dp),
                                 progressColor = Color(0xFF6633CC),
                                 progressBackgroundColor = Color(0xFFE0E0E0),
-                                indicators = listOf(0.3f,0.5f,0.7f,0.85f),
+                                //indicators = listOf(0.3f,0.5f,0.7f,0.85f),
                                 fillProgressBy = 0.95f,
-                                cornerRadius = CornerRadius(30f,30f)
+                                cornerRadius = CornerRadius(30f, 30f)
+                            )
+                            StepProgress(
+                                modifier = Modifier
+                                    .height(30.dp)
+                                    .width(200.dp),
+                                cornerRadius = CornerRadius(30f, 30f),
+                                progressColor = Color(0xFF6633CC),
+                                progressBackgroundColor = Color(0xFFE0E0E0),
+                                currentStep = 6,
+                                totalStep = 6,
+                                stepImageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_check_24)
                             )
                         }
                     }
